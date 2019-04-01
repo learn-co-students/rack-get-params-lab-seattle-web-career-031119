55231ca30444fffec87c8c123a10f009adf01ff5
quinncidences <qrc.inpa@gmail.com>
2019-04-01 09:36:53 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,15 +11,28 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      resp.write cart_search(resp)
+
+    elsif req.path.match(/add/)
+      search_term = req.params["item"]
+
+      resp.write cart_action(search_term)
+
     else
       resp.write "Path Not Found"
+
     end
 
     resp.finish
   end
+
+
 
   def handle_search(search_term)
     if @@items.include?(search_term)
@@ -27,4 +41,28 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  def cart_search(resp)
+    if @@cart.empty?
+      return "Your cart is empty"
+    else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    end
+
+  end
+
+  def cart_action(search_term)
+    if @@items.include?(search_term)
+      @@cart << search_term
+      return "added #{search_term}"
+    elsif !@@items.include?(search_term)
+      return "We don't have that item"
+    end
+
+  end
+
 end
+
+# if !@@cart.include?(search_term) &&
